0e20d5be131b2af3cafca33e5014bf9b8e512569
Esther-Omono <enitanatakere@gmail.com>
2025-02-19 05:33:17 +0100

    Tweaked the padding and margin a bit

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -7,13 +7,13 @@ export const Message = ({ message }) => {
     2. Style for disabled feature
     3. Add aria labels for accesibility
      */
-    <>
+    <div className='mb-8'>
       <div className='bg-[rgba(39,174,96,0.7)] p-4 rounded-lg max-w-[90%] m-auto self-start mb-4'>
         <p className='mb-2 break-words text-white'>{message}</p>
         <p className='text-sm text-gray-200 italic mb-2'>Detected lanuage:</p>
       </div>
 
-      <div className='max-w-[90%] m-auto flex justify-between flex-wrap gap-4 my-4 md:flex-row'>
+      <div className='max-w-[90%] m-auto flex justify-between flex-wrap gap-4 -my-2 md:flex-row'>
         <button className='bg-[rgba(39,174,96,0.7)] text-white py-2 px-4 rounded text-sm transition duration-300 cursor-pointer'>
           Summarize
         </button>
@@ -34,7 +34,7 @@ export const Message = ({ message }) => {
         </div>
       </div>
 
-      <div className='mt-6 p-4 max-w-[90%] m-auto bg-[#283335] rounded'>
+      <div className='mt-4 p-4 max-w-[90%] m-auto bg-[#283335] rounded -my-4'>
         <h3 className='text-sm font-medium text-gray-400 mb-1'>Summary:</h3>
         <p className='text-gray-300'>Summarized message</p>
       </div>
@@ -43,7 +43,7 @@ export const Message = ({ message }) => {
         <h3 className='text-sm font-medium text-gray-400 mb-1'>Translation</h3>
         <p className='text-gray-300'>Translated Message</p>
       </div>
-    </>
+    </div>
   );
 };
 
